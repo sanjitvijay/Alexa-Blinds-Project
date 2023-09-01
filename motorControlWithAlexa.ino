94ef31285aed1f4939a20ddc9f667f9cb8fe793e
Sanjit <sanjit.vijay@outlook.com>
2023-09-01 16:47:32 -0400

    Add files via upload

---
+++ motorControlWithAlexa.ino
@@ -0,0 +1,113 @@
+/*
+ * Simple example for how to use multiple SinricPro Switch device:
+ * - setup 4 switch devices
+ * - handle request using multiple callbacks
+ * 
+ * If you encounter any issues:
+ * - check the readme.md at https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md
+ * - ensure all dependent libraries are installed
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#arduinoide
+ *   - see https://github.com/sinricpro/esp8266-esp32-sdk/blob/master/README.md#dependencies
+ * - open serial monitor and check whats happening
+ * - check full user documentation at https://sinricpro.github.io/esp8266-esp32-sdk
+ * - visit https://github.com/sinricpro/esp8266-esp32-sdk/issues and check for existing issues or open a new one
+ */
+
+
+#ifdef ENABLE_DEBUG
+   #define DEBUG_ESP_PORT Serial
+   #define NODEBUG_WEBSOCKETS
+   #define NDEBUG
+#endif 
+
+#include <Arduino.h>
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ESP32) || defined(ARDUINO_ARCH_RP2040)
+  #include <WiFi.h>
+#endif
+
+#include "SinricPro.h"
+#include "SinricProSwitch.h"
+#include <L298N.h>
+
+#define WIFI_SSID         "Vijay"
+#define WIFI_PASS         "manicsea132"
+#define APP_KEY           "0533bae5-513d-4a01-b67e-3b968d41f1d9"
+#define APP_SECRET        "230d0c71-5531-413c-bb46-5bc87a0af1d6-fd884712-0516-4d9c-9da6-b60a137b893e"
+
+#define SWITCH_ID_1       "64c2e5512ac6a1822a944e44"
+#define RELAYPIN_1        LED_BUILTIN
+
+#define BAUD_RATE         9600                // Change baudrate to your need
+
+#define EN 15
+#define IN1 13
+#define IN2 12
+
+L298N myMotor(EN,IN1,IN2);
+
+bool onPowerState1(const String &deviceId, bool &state) {
+ Serial.printf("Device 1 turned %s", state?"on":"off");
+ digitalWrite(RELAYPIN_1, state ? LOW:HIGH);
+ if(!state){
+   myMotor.backward();
+   delay(1000);
+   myMotor.stop(); 
+ }else{
+   myMotor.forward();
+   delay(1000);
+   myMotor.stop();
+ }
+ return true; // request handled properly
+}
+
+// setup function for WiFi connection
+void setupWiFi() {
+  Serial.printf("\r\n[Wifi]: Connecting");
+
+  #if defined(ESP8266)
+    WiFi.setSleepMode(WIFI_NONE_SLEEP); 
+    WiFi.setAutoReconnect(true);
+  #elif defined(ESP32)
+    WiFi.setSleep(false); 
+    WiFi.setAutoReconnect(true);
+  #endif
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.printf(".");
+    delay(250);
+  }
+
+  Serial.printf("connected!\r\n[WiFi]: IP-Address is %s\r\n", WiFi.localIP().toString().c_str());
+}
+
+// setup function for SinricPro
+void setupSinricPro() {
+  // add devices and callbacks to SinricPro
+  pinMode(RELAYPIN_1, OUTPUT);
+    
+  SinricProSwitch& mySwitch1 = SinricPro[SWITCH_ID_1];
+  mySwitch1.onPowerState(onPowerState1);
+  
+  
+  // setup SinricPro
+  SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); }); 
+  SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
+   
+  SinricPro.begin(APP_KEY, APP_SECRET);
+}
+
+// main setup function
+void setup() {
+  myMotor.setSpeed(200);
+  Serial.begin(BAUD_RATE); Serial.printf("\r\n\r\n");
+  setupWiFi();
+  setupSinricPro();
+}
+
+void loop() {
+  SinricPro.handle();
+}
